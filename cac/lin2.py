1373e1d4a600387cf1c3b15b7b30c9e30056bf99
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2017-08-03 17:54:08 -0700

    add polyfits to curves

--- cac/lin2.py
+++ cac/lin2.py
@@ -45,32 +45,74 @@ for path in pathlist:
     os.chdir(pwd)
 
 # px = px[20:23, :]
+TOP_BTM_DIFF = 5000
+indexes = np.zeros(px.shape[0], dtype=int)
+for r in range(px.shape[0]):
+    diffs = np.ediff1d(px[r, :].astype(np.int))
+    peak = np.where(abs(diffs) > TOP_BTM_DIFF)[0]
+    if peak.shape[0] > 1:  # multiple peaks
+        peak = peak[0]  # take first one
+    indexes[r] = peak
+
 # Align peaks
 if APEAK:
-    logging.info("min size is %d", min_px)
-    TOP_BTM_DIFF = 5000
-    indexes = np.zeros(px.shape[0], dtype=int)
-    for r in range(px.shape[0]):
-        diffs = np.ediff1d(px[r, :].astype(np.int))
-        peak = np.where(abs(diffs) > TOP_BTM_DIFF)[0]
-        if peak.shape[0] > 1:  # multiple peaks
-            peak = peak[0]  # take first one
-        indexes[r] = peak
-
     # we can pad or roll
     rollval = max(indexes) - indexes
     for r in range(px.shape[0]):
         px[r, :] = np.roll(px[r, :], rollval[r])
 
-px_avg = np.average(px, 0)
-px_std = np.std(px, 0)
-
-# Add std deviation bars to the previous plot
-
-plt.figure()
+# plot average across multiple runs
 plt.title('%s\n%s runs Linearity Test Pixel (%d,%d), tr1 00' % (filename, str(px.shape[0]),
           row, col))
-plt.errorbar(np.arange(min_px), px_avg, yerr=px_std, fmt='--.',  ecolor='r')
+# px_avg = np.average(px, 0)
+# px_std = np.std(px, 0)
+# plt.errorbar(np.arange(min_px), px_avg, yerr=px_std, fmt='--.',  ecolor='r')
+# plt.show()
+
+# plot poly fits
+# create axis
+x = np.arange(min_px)
+rampstart = 660
+rampstop = max(indexes)+1
+# fitstart = max(indexes)+1
+# fitstart = 870
+fitstart = rampstop
+
+# tail after autoranging switch. we are only interested in this range
+rx = x[rampstart:rampstop]
+ry = px[:, rampstart:rampstop]
+
+# repeat x for all observations
+rxflat = np.tile(rx, px.shape[0])
+ryflat = ry.ravel()  # flatten y-axis for multiple observations
+
+rcoeff = np.polyfit(rxflat, ryflat, 1)  # first order polyfit or ramp
+rfit1 = np.poly1d(rcoeff)
+
+# tail after autoranging switch. we are only interested in this range
+tx = x[fitstart:]
+ty = px[:, fitstart:]
+
+# repeat x for all observations
+txflat = np.tile(tx, px.shape[0])
+tyflat = ty.ravel()  # flatten y-axis for multiple observations
+
+tcoeff = np.polyfit(txflat, tyflat, 1)  # first order polyfit or tail
+tfit1 = np.poly1d(tcoeff)
+
+plt.plot(px.T, 'k.')
+plt.plot(rx, rfit1(rx), '-', linewidth=3)
+plt.plot(tx, tfit1(tx), '-', linewidth=3)
+
+newy = rfit1(rampstop)
+newc = newy - tcoeff[0] * rampstop
+newtcoeff = tcoeff.copy()
+newtcoeff[1] = newc
+newtfit1 = np.poly1d(newtcoeff)
+newty = ty+newtfit1(tx)-tfit1(tx)
+plt.plot(txflat, newty.ravel(), '.', color='#808080', alpha=.15)
+plt.plot(tx, newtfit1(tx), '-', linewidth=3)
 plt.show()
-plt.plot(px.T)
-plt.show()
+
+print(rfit1)
+print(tfit1)
